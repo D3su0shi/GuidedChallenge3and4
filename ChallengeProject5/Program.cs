a4e2f7bd36429a08412b99d18f4879864c86eb02
dylan yagninim <dylanmagnym@gmail.com>
2025-09-18 10:05:00 -0400

    Create a method to shuffle an array

--- ChallengeProject5/Program.cs
+++ ChallengeProject5/Program.cs
@@ -6,3 +6,22 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
+
+void RandomizeAnimals() 
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
